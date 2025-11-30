9fc1d77238fc337cf783b05e39bba50f4362671f
nswarnob <nswarnob@gmail.com>
2025-12-01 02:58:46 +0600

    pool connection create

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,59 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
 const app = express();
 const port = 5000;
 
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos(
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES Users(id) ON DELETE CASCADE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+};
+
+initDB();
+
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req);
+  res.status(201).json({
+    success: true,
+    message: "Api is working.",
+  });
 });
 
 app.listen(port, () => {
