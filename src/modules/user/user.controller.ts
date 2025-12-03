eb0fbf6aa3df35c1c7ea233307145c03b1df19ca
nswarnob <nswarnob@gmail.com>
2025-12-04 01:59:47 +0600

    ..

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -19,6 +19,22 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export const userControllers = {
-  createUser,
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getUser();
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 }
+
+export const userControllers = {
+  createUser, getUser
+};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -8,6 +8,12 @@ const createUser = async (name: string, email: string) => {
   return result;
 };
 
+const getUser = async () => {
+  const result = await pool.query(`SELECT * FROM users`);
+  return result;
+};
+
 export const userServices = {
   createUser,
+  getUser,
 };
